86d49fca20b03d16fba352d27241165d4896b291
SWKANG0525 <stardustsw8947@gmail.com>
2020-04-01 04:55:57 +0900

    Updates

---
+++ precache-manifest.b9d0548b3ca9e3029dd7341d1f66b9b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee178f2587caa1402e9c64a58cf2c5bd",
+    "url": "/portfolio_react/index.html"
+  },
+  {
+    "revision": "a5c2b3f4c3ffec44e615",
+    "url": "/portfolio_react/static/css/main.236ca438.chunk.css"
+  },
+  {
+    "revision": "cdb1a1381154ecd7b1ca",
+    "url": "/portfolio_react/static/js/2.08bfbaa1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio_react/static/js/2.08bfbaa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c2b3f4c3ffec44e615",
+    "url": "/portfolio_react/static/js/main.26d4244b.chunk.js"
+  },
+  {
+    "revision": "03072cc91dad35a750be",
+    "url": "/portfolio_react/static/js/runtime-main.493313f0.js"
+  }
+]);
